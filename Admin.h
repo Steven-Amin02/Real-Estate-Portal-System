9d8abccd09ba6bae585a6ae1bafa468546b46b9f
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2025-05-07 23:51:21 +0300

    Update Admin.h

--- Admin.h
+++ Admin.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #include <iostream>
 #include<vector>
@@ -17,12 +16,13 @@ struct DashboardStats {
 	size_t pendingUserApprovals;
 	size_t pendingPropertyApprovals;
 };
-class Admin {
+class Admin: public BaseAccount {
 	//baseaccount pointer for efficient storage pointer only store the memory address 
 	unordered_map<string, BaseAccount*> Users;
     unordered_map<int, PropertyListing> Property_Listing;
     set<int> highlightedProperty;
-	queue<PropertyListing> pendingApprovals;
+	//queue<PropertyListing> pendingApprovals;
+	queue<int> pendingApprovals;
 
 public:
 	Admin(string username, string password, string email, string phone);
@@ -31,21 +31,22 @@ public:
 	void UserManagement();
 
 	void viewPendingUserApprovals() ;
-
+	void approveUser(string userHandle);
 
 	DashboardStats getDashboardStats();
 	void accessDashboard();
 
 	void addPropertyToApprovalQueue(PropertyListing property);
 
+	
 private:
-
+	
 	void approveNextProperty();
 	void rejectNextProperty();
 
 	void addPropertyListing(PropertyListing property);
 	void removePropertyListings(int propertyid);
-	void editPropertyListing(int propertyid, PropertyListing updatepropertylisting);
+	void editPropertyListing(int propertyid, PropertyListing& updatepropertylisting);
 	void highlightPropertyListing(int propertyid);
-
+	void displayAllProperty();
 };

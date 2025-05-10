e79a4f0937741663b81eb186f387c0bd74720a21
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2025-05-10 16:10:03 +0300

    Update searchAndCompare.h

--- searchAndCompare.h
+++ searchAndCompare.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>
 
 
 struct PropertyHash {
@@ -18,10 +19,24 @@ struct PropertyEqual {
 };
 class searchAndCompare {
 public:
-	vector<PropertyListing> searchByType( unordered_map<int, PropertyListing>& properties, string type);
-	vector<PropertyListing> searchBypriceRange( unordered_map<int, PropertyListing>& properties, double minPrice, double maxPrice);
-	vector<PropertyListing> searchByAreaRange( unordered_map<int, PropertyListing>& properties, double minArea, double maxArea);
-	vector<PropertyListing> searchByLocation( unordered_map<int, PropertyListing>& properties, string government, string city);
-    unordered_set<PropertyListing, PropertyHash, PropertyEqual> searchMerge(vector<PropertyListing> type,vector<PropertyListing> price,vector<PropertyListing> area,vector<PropertyListing> location);
+    vector<PropertyListing> searchByType(unordered_map<int, PropertyListing>& properties, string type);
+    vector<PropertyListing> searchBypriceRange(unordered_map<int, PropertyListing>& properties, double minPrice, double maxPrice);
+    vector<PropertyListing> searchByAreaRange(unordered_map<int, PropertyListing>& properties, double minArea, double maxArea);
+    vector<PropertyListing> searchByLocation(unordered_map<int, PropertyListing>& properties, string government, string city);
+    vector<PropertyListing> searchByBedrooms(unordered_map<int, PropertyListing>& properties,int bedrooms);
+    unordered_set<PropertyListing, PropertyHash, PropertyEqual> searchMerge(vector<PropertyListing> type, vector<PropertyListing> price, vector<PropertyListing> area, vector<PropertyListing> location);
     void searchMenu();
+
+    static bool comparePriceAsc(const PropertyListing& a, const PropertyListing& b);
+    static bool comparePriceDesc(const PropertyListing& a, const PropertyListing& b);
+    static bool compareSizeAsc(const PropertyListing& a, const PropertyListing& b);
+    static bool compareSizeDesc(const PropertyListing& a, const PropertyListing& b);
+
+    vector<PropertyListing> sortByPriceAsc(unordered_map<int, PropertyListing>& properties);
+    vector<PropertyListing> sortByPriceDesc(unordered_map<int, PropertyListing>& properties);
+
+    vector<PropertyListing> sortBySizeAsc(unordered_map<int, PropertyListing>& properties);
+    vector<PropertyListing> sortBySizeDesc(unordered_map<int, PropertyListing>& properties);
+
+    void compareProperties(unordered_map<int, PropertyListing>& properties);
 };

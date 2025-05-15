2509fe28dfd3b1c5a38a56c4a0b665fd6541bece
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2025-05-15 19:11:45 +0300

    Update Admin.cpp

--- final/Admin.cpp
+++ final/Admin.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 
 Admin::Admin(std::string uname, std::string pwd, std::string n)
-    : User(uname, pwd, n) {}
+    : User(uname, pwd, n) {
+}
 
 void Admin::displayMenu()
 {
-  std::cout << "\n--- Admin Dashboard ---\n";
-  std::cout << "1. Manage Listings\n";
-  std::cout << "2. Manage Users\n";
-  std::cout << "3. View All Properties\n";
-  std::cout << "4. Logout\n";
+    std::cout << "\n--- Admin Dashboard ---\n";
+    std::cout << "1. Manage Listings\n";
+    std::cout << "2. Manage Users\n";
+    std::cout << "3. View All Properties\n";
+    std::cout << "4. Logout\n";
 }

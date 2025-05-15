ab5fdf99835805bb76f56513866c859144895141
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2025-05-15 19:11:13 +0300

    Update RegularUser.h

--- final/RegularUser.h
+++ final/RegularUser.h
@@ -4,6 +4,6 @@
 class RegularUser : public User
 {
 public:
-  RegularUser(std::string uname, std::string pwd, std::string n);
-  void displayMenu() override;
+	RegularUser(std::string uname, std::string pwd, std::string n);
+	void displayMenu() override;
 };

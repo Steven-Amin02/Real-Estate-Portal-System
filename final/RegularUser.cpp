155cb616632a0ebc740b56051578062f2b3c29da
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2025-05-15 19:12:35 +0300

    Update RegularUser.cpp

--- final/RegularUser.cpp
+++ final/RegularUser.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 
 RegularUser::RegularUser(std::string uname, std::string pwd, std::string n)
-    : User(uname, pwd, n) {}
+    : User(uname, pwd, n) {
+}
 
 void RegularUser::displayMenu()
 {
-  std::cout << "\n--- User Menu ---\n";
-  std::cout << "1. List a Property\n";
-  std::cout << "2. Search Properties\n";
-  std::cout << "3. Compare Properties\n";
-  std::cout << "4. View My Listings\n";
-  std::cout << "5. Logout\n";
+    std::cout << "\n--- User Menu ---\n";
+    std::cout << "1. List a Property\n";
+    std::cout << "2. Search Properties\n";
+    std::cout << "3. Compare Properties\n";
+    std::cout << "4. View My Listings\n";
+    std::cout << "5. Logout\n";
 }

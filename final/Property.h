3911ffeb2daefe9812f7d03fd8ab44d6fed2ea0b
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2025-05-15 19:10:41 +0300

    Update Property.h

--- final/Property.h
+++ final/Property.h
@@ -3,26 +3,29 @@
 
 class Property
 {
-  int id;
-  std::string name, location, type;
-  double price;
-  std::string features;
-  std::string ownerUsername;
-  bool highlighted = false;
-  bool approved = false;
+	int id;
+	std::string name, location, type;
+	double price;
+	std::string features;
+	std::string ownerUsername;
+	bool highlighted = false;
+	bool approved = false;
 
 public:
-  Property(int id, std::string n, std::string loc, std::string t, double p, std::string f, std::string owner, bool highlight = false, bool approve = false);
-  void display() const;
-  int getId() const;
-  std::string getLocation() const;
-  double getPrice() const;
-  std::string getType() const;
-  std::string getName() const;
-  std::string getFeatures() const;
-  std::string getOwnerUsername() const;
-  bool isHighlighted() const;
-  void setHighlighted(bool h);
-  bool isApproved() const;
-  void setApproved(bool a);
+	// Default constructor
+	Property() : id(0), price(0.0) {}
+
+	Property(int id, std::string n, std::string loc, std::string t, double p, std::string f, std::string owner, bool highlight = false, bool approve = false);
+	void display() const;
+	int getId() const;
+	std::string getLocation() const;
+	double getPrice() const;
+	std::string getType() const;
+	std::string getName() const;
+	std::string getFeatures() const;
+	std::string getOwnerUsername() const;
+	bool isHighlighted() const;
+	void setHighlighted(bool h);
+	bool isApproved() const;
+	void setApproved(bool a);
 };
